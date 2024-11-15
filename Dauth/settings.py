28a427a1907836deb707dc001c4b3cbe6ac2f9ec
Ruddy <rudraparbat714@gmail.com>
2024-11-16 02:19:08 +0530

    postgres added sucessfully

--- Dauth/settings.py
+++ Dauth/settings.py
@@ -93,6 +93,22 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',  
+        'USER': 'postgres',  
+        'PASSWORD': 'postgres',  
+        'HOST': 'db',  
+        'PORT': 5432,  
+    }
+}
 
 
 # Password validation
