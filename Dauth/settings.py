0556b3108c6cce23c99c96dc5c5ebed93d03a7f6
Ruddy <rudraparbat714@gmail.com>
2024-11-16 20:44:44 +0530

    added test cases

--- Dauth/settings.py
+++ Dauth/settings.py
@@ -99,16 +99,16 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',  
-        'USER': 'postgres',  
-        'PASSWORD': 'postgres',  
-        'HOST': 'db',  
-        'PORT': 5432,  
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',  
+#         'USER': 'postgres',  
+#         'PASSWORD': 'postgres',  
+#         'HOST': 'db',  
+#         'PORT': 5432,  
+#     }
+# }
 
 
 # Password validation
--- authers/pytests/test_endpoints.py
+++ authers/pytests/test_endpoints.py
@@ -1,19 +1,12 @@
 import requests
+import pytest
+from django.test import Client
 endpoint = 'http://127.0.0.1:8000/api'
-def test_logout_user() :
-    response = requests.get(endpoint+'/logout')
-    assert response.status_code == 200
-def test_login_user() :
-    # test with default username and password which is already saved in database
-    response = requests.post(endpoint+'/login/',json={
-        "username" : "ruddy",
-        "password" : 12345
-    })
-    assert response.status_code == 200
+
 def test_register_user() :
     response = requests.post(endpoint+'/register/' ,json={
         "username" : "justin",    # you can add your username
-        "password" : "Bieber@200", # you can add your password but must me in this pattern 
+        "password" : "Bieber@200", # you can add your password but must be in this pattern 
         "date_of_birth" : "21-12-2003" , # you cann add tour birth date
         "phone_numbers" : 9789343431 , # also phone number but not a string include as a phone number
         "email" : "jsutin@gmail.com" # also add your eamil in string format
@@ -23,6 +16,22 @@ def test_register_user() :
         assert response.status_code == 400
     else :
         assert response.status_code == 201
+
+def test_login_user(db) :
+    client = Client()
+    # test with default username and password which is already saved in database
+    response = requests.post(endpoint+'/login/',json={
+        "username" : "ruddy",
+        "password" : 12345
+    })
+    response_body = response.json()
+    assert response.status_code == 200
+    session = client.session
+    session['access_token'] = response_body['access_token']
+    session['refresh_token'] = response_body['refresh']
+    session.save()
+
+
 def test_get_user() :
     response = requests.get(endpoint+'/profile')
     if response.status_code == 401 :
@@ -30,3 +39,8 @@ def test_get_user() :
         assert response.status_code == 401
     else :
         assert response.status_code == 200
+
+
+def test_logout_user() :
+    response = requests.get(endpoint+'/logout')
+    assert response.status_code == 200
